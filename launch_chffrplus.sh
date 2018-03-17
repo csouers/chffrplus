91e40b3a9a6092340805202f4e41dc12aa537892
Chris Souers <csouers@gmail.com>
2018-03-17 01:14:59 -0500

    stops auto update and panda flashing

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -2,17 +2,17 @@
 
 function launch {
   # apply update
-  if [ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]; then
-     git reset --hard @{u} &&
-     git clean -xdf &&
-     exec "${BASH_SOURCE[0]}"
-  fi
+#  if [ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]; then
+#     git reset --hard @{u} &&
+#     git clean -xdf &&
+#     exec "${BASH_SOURCE[0]}"
+#  fi
 
   # check if NEOS update is required
-  while [ "$(cat /VERSION)" -lt 4 ] && [ ! -e /data/media/0/noupdate ]; do
-    curl -o /tmp/updater https://neos.comma.ai/updater && chmod +x /tmp/updater && /tmp/updater
-    sleep 10
-  done
+#  while [ "$(cat /VERSION)" -lt 4 ] && [ ! -e /data/media/0/noupdate ]; do
+#    curl -o /tmp/updater https://neos.comma.ai/updater && chmod +x /tmp/updater && /tmp/updater
+#    sleep 10
+#  done
 
   # no cpu rationing in chffrplus
   echo 0-3 > /dev/cpuset/background/cpus
